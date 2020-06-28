b49b57796a0dae5cec93988ffa2cb7e63c47f68c
Louis Kinley <louis@caripay.net>
2020-06-28 14:55:34 -0400

    applied kvonkoni:kvonkoni/issue72

--- dicttoxml.py
+++ dicttoxml.py
@@ -4,7 +4,7 @@
 """
 Converts a Python dictionary or other native data type into a valid XML string. 
 
-Supports item (`int`, `float`, `long`, `decimal.Decimal`, `bool`, `str`, `unicode`, `datetime`, `none` and other number-like objects) and collection (`list`, `set`, `tuple` and `dict`, as well as iterable and dict-like objects) data types, with arbitrary nesting for the collections. Items with a `datetime` type are converted to ISO format strings. Items with a `None` type become empty XML elements.
+Supports item (`int`, `float`, `long`, `decimal.Decimal`, `bool`, `str`, `unicode`, `datetime`, `none` and other number-like objects) and collection (`list`, `set`, `tuple` and `dict`, as well as iterable and dict-like objects) data types, with arbitrary nesting for the collections.abc. Items with a `datetime` type are converted to ISO format strings. Items with a `None` type become empty XML elements.
 
 This module works with both Python 2 and 3.
 """
@@ -15,7 +15,7 @@ __version__ = '1.7.4'
 version = __version__
 
 from random import randint
-import collections
+import collections.abc
 import numbers
 import logging
 from xml.dom.minidom import parseString
@@ -97,7 +97,7 @@ def get_xml_type(val):
         return 'null'
     if isinstance(val, dict):
         return 'dict'
-    if isinstance(val, collections.Iterable):
+    if isinstance(val, collections.abc.Iterable):
         return 'list'
     return type(val).__name__
 
@@ -189,7 +189,7 @@ def convert(obj, ids, attr_type, item_func, cdata, parent='root'):
     if isinstance(obj, dict):
         return convert_dict(obj, ids, parent, attr_type, item_func, cdata)
         
-    if isinstance(obj, collections.Iterable):
+    if isinstance(obj, collections.abc.Iterable):
         return convert_list(obj, ids, parent, attr_type, item_func, cdata)
         
     raise TypeError('Unsupported data type: %s (%s)' % (obj, type(obj).__name__))
@@ -231,7 +231,7 @@ def convert_dict(obj, ids, parent, attr_type, item_func, cdata):
                 )
             )
 
-        elif isinstance(val, collections.Iterable):
+        elif isinstance(val, collections.abc.Iterable):
             if attr_type:
                 attr['type'] = get_xml_type(val)
             addline('<%s%s>%s</%s>' % (
@@ -294,7 +294,7 @@ def convert_list(items, ids, parent, attr_type, item_func, cdata):
                     )
                 )
 
-        elif isinstance(item, collections.Iterable):
+        elif isinstance(item, collections.abc.Iterable):
             if not attr_type:
                 addline('<%s %s>%s</%s>' % (
                     item_name, make_attrstring(attr), 
